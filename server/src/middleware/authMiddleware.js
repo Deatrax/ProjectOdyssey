b7c7da2e8b1167639dc70a2e608ab4abbc62a288
Tasnif Emran <tasnifemran@gmail.com>
2025-12-12 21:41:46 +0600

    Basic User DB created. Waiting for complete schema. APIs created for signup, login and get profile. Login with JWT token. Bcrypt password encrytption added.

---
+++ server/src/middleware/authMiddleware.js
@@ -0,0 +1,21 @@
+const jwt = require("jsonwebtoken");
+
+const protect = (req, res, next) => {
+  let token = req.headers.authorization;
+
+  if (!token) return res.status(401).json({ message: "No token, authorization denied" });
+
+  if (token.startsWith("Bearer ")) token = token.slice(7, token.length);
+
+  try {
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    req.user = decoded; // attach user info to request
+    next();
+  } catch (err) {
+    return res.status(401).json({ message: "Token is not valid" });
+  }
+};
+
+module.exports = protect;
+
+// its job is to protect routes so only users with a valid JWT can access them.
--- server/src/server.js
+++ server/src/server.js
@@ -2,12 +2,22 @@ require('dotenv').config()
 const express = require('express')
 const mongoose = require('mongoose');
 const connectDB = require('./config/db');
-const app = express();
-app.use(express.json())
+const authRoutes = require('./routes/auth'); 
+const protectedRoutes = require("./routes/protected");
 
-const PORT = process.env.PORT || 5001
+const app = express();
+app.use(express.json());
+
+
+// Connect mongoDB
 connectDB();
 
+// Routes
+app.use('/api/auth', authRoutes);
+app.use('/api/user', protectedRoutes);
+
+// Connect PORT
+const PORT = process.env.PORT || 5001;
 app.listen(PORT, () => {
    console.log("Server started on PORT: " + PORT);
 })
