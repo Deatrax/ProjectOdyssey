ab4fdb75b4837f66d08a350596b888fc2a1fbe45
Tasnif Emran <tasnifemran@gmail.com>
2025-12-20 00:18:01 +0600

    APIs modified based on the frontend requirements.

--- server/src/models/User.js
+++ server/src/models/User.js
@@ -11,6 +11,16 @@ const userSchema = new mongoose.Schema({
     type: String,
     required: true,
   },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    trim: true
+  },
+  dob: {
+    type: Date, 
+    required: true
+  }
 });
 
 // Hash password before saving
