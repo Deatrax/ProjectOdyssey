1d93905a94702b04c188d52a05d3dd3ef76c1892
Deatrax <161139217+Deatrax@users.noreply.github.com>
2025-12-20 19:59:49 +0600

    did the converstions and updates

--- client/odyssey/app/signup/page.tsx
+++ client/odyssey/app/signup/page.tsx
@@ -3,6 +3,7 @@
 import React, { useState } from "react";
 import { useRouter } from "next/navigation";
 import Link from "next/link";
+import "./landingPage.css";
 
 const SignupPage: React.FC = () => {
   const router = useRouter();
