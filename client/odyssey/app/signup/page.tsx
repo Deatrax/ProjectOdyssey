6bae73ef043380925b2758d5ed3ad6a550998df9
alfishahrin-05 <alfishahrin05@gmail.com>
2026-01-18 23:15:42 +0600

    Correction of Port Number for manual account creation and sign in

--- client/odyssey/app/signup/page.tsx
+++ client/odyssey/app/signup/page.tsx
@@ -39,7 +39,7 @@ const SignupPage: React.FC = () => {
 
     try {
       // Connect to your Backend
-      const res = await fetch("http://127.0.0.1:4000/api/auth/signup", {
+      const res = await fetch("http://localhost:5001/api/auth/signup", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({
